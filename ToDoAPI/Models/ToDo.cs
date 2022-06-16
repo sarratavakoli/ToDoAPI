290bb4c6fce8468f85cdb3f0f9cecd81ac9d4d30
sarratavakoli <sarra.tavakoli@gmail.com>
2022-06-15 21:49:20 -0500

    Add project files.

---
+++ ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoAPI.Models
+{
+    public partial class ToDo
+    {
+        public int ToDoId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Details { get; set; }
+        public bool Done { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+    }
+}
---
+++ ToDoAPI/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:7105");
+        });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//this will initialize the db connection to be used in the controllers
+builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDB"));
+    }
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
