8bb6f7825e392829e361227820aa5d11a2ccaef9
sarratavakoli <sarra.tavakoli@gmail.com>
2022-06-15 22:18:03 -0500

    Enabled CORS on controllers

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:7105");
+            policy.WithOrigins("http://localhost:3002");
         });
 });
 
